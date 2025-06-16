539ac4b09bda6fa327266dcc9380e86437449b86
Vanshika Goyal <goyalvanshika04@gmail.com>
2025-06-16 11:37:17 +0530

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/api')
-def message():
+def hello():
     return jsonify({"message": "Hello from Azure!"})
 
 if __name__ == '__main__':
